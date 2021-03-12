e669f0d244999d1f8bf4362ff5ab469375d00ab7
gceylan <gulsumceylan1998@gmail.com>
2021-03-12 14:10:12 +0300

    16. gün sonu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { RentalComponent } from './components/rental/rental.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    ColorComponent,
+    BrandComponent,
+    CustomerComponent,
+    RentalComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
