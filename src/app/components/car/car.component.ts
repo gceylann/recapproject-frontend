3d7d71cfb8378c25f4b819d0a7b3ffc81e886796
gceylan <gulsumceylan1998@gmail.com>
2021-03-13 14:18:53 +0300

    17.gün

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/car';
-import {HttpClient } from '@angular/common/http';
+
 import { CarResponseModel } from 'src/app/models/carResponseModel';
+import { CarService } from 'src/app/services/car.service';
 
 @Component({
   selector: 'app-car',
@@ -10,21 +11,18 @@ import { CarResponseModel } from 'src/app/models/carResponseModel';
 })
 export class CarComponent implements OnInit {
 
-
-  cars:Car[] = []
-  apiUrl = "https://localhost:44308/api/cars";
-  constructor(private httpClient:HttpClient) { }
+  cars:Car[] = [];
+  
+  constructor(private CarService:CarService) { }
 
   ngOnInit(): void {
     this.getCars();
   }
 
   getCars(){
-    this.httpClient
-    .get<CarResponseModel>(this.apiUrl)
-    .subscribe((response)=> {
-      this.cars= response.data
-    });
+    this.CarService.getCars().subscribe(response=>{
+      this.cars = response.data
+    })
 
   }
 
